a8250754df5526f67bea61c6863ff901bb22e838
Krishnakumar R <krishna4u.rn@gmail.com>
2026-08-04 09:49:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = "Welcome";
+include 'includes/header.php';
+include 'includes/navbar.php';
+?>
+
+<h2>Welcome to Expense Tracker</h2>
+<p>This app helps you manage personal and group expenses easily.</p>
+<p><a href="register.php">Register</a> or <a href="login.php">Login</a> to get started.</p>
+
+<?php include 'includes/footer.php'; ?>
