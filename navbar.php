d0c6e3cebebceb682b6809152b6dd8e277f9890c
Krishnakumar R <krishna4u.rn@gmail.com>
2026-08-11 16:31:59 +0530

    navbar

---
+++ navbar.php
@@ -0,0 +1,6 @@
+<nav class="navbar">
+  <a href="dashboard.php">Dashboard</a>
+  <a href="group.php">Groups</a>
+  <a href="add_expense.php">Add Expense</a>
+  <a href="logout.php">Logout</a>
+</nav>
